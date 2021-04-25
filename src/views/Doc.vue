586cc555d045105dc00d3b44037029a426292c98
liu zhenbin <536543269@qq.com>
2021-04-25 17:26:09 +0800

    实现切换

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -2,7 +2,7 @@
   <div>
     <TopNav />
     <div class="content">
-      <aside>
+      <aside v-if="menuVisible">
         <h2>组件列表</h2>
         <ol>
           <li>
@@ -25,8 +25,13 @@
 </template>
 <script lang="ts">
 import TopNav from "../components/TopNav.vue";
+import { inject, Ref } from "vue";
 export default {
   components: { TopNav },
+  setup() {
+    const menuVisible = inject<Ref<boolean>>("menuVisible"); // get
+    return { menuVisible };
+  },
 };
 </script>
 <style lang="scss" scoped>
@@ -37,6 +42,7 @@ aside {
   position: fixed;
   top: 0;
   left: 0;
+  padding-top: 70px;
   > h2 {
     margin-bottom: 4px;
   }
