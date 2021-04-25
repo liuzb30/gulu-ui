49304ea6a297a587248bce6f844c8f7f7488fab6
liu zhenbin <536543269@qq.com>
2021-04-25 17:14:16 +0800

    封装TopNav组件

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,6 @@
 <template>
   <div>
-    <div class="topnav">
-      <div class="logo"></div>
-      <div class="menu"></div>
-    </div>
+    <TopNav />
     <div class="banner">
       <h1>轱辘UI</h1>
       <h2>一个厉害的 UI 框架</h2>
@@ -14,22 +11,9 @@
     </div>
   </div>
 </template>
-<style lang="scss" scoped>
-.topnav {
-  background: pink;
-  display: flex;
-  padding: 16px;
-  > .logo {
-    max-width: 6em;
-    margin-right: auto;
-  }
-  > .menu {
-    display: flex;
-    white-space: nowrap;
-    flex-wrap: nowrap;
-    > li {
-      margin: 0 1em;
-    }
-  }
-}
-</style>
+<script lang="ts">
+import TopNav from "../components/TopNav.vue";
+export default {
+  components: { TopNav },
+};
+</script>
