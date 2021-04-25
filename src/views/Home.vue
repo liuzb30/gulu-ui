2e01c9ac2c766799c69cd30992a39b4ac30271d8
liu zhenbin <536543269@qq.com>
2021-04-25 17:11:11 +0800

    topnav样式更新

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,3 +14,22 @@
     </div>
   </div>
 </template>
+<style lang="scss" scoped>
+.topnav {
+  background: pink;
+  display: flex;
+  padding: 16px;
+  > .logo {
+    max-width: 6em;
+    margin-right: auto;
+  }
+  > .menu {
+    display: flex;
+    white-space: nowrap;
+    flex-wrap: nowrap;
+    > li {
+      margin: 0 1em;
+    }
+  }
+}
+</style>
