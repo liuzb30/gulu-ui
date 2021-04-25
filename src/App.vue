a3091ba647b1d807736b16ff91c10942f60e766d
liu zhenbin <536543269@qq.com>
2021-04-25 17:41:09 +0800

    切换路由关闭aside

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
 
 <script lang="ts">
 import { ref, provide } from "vue";
+import router from "./router";
 
 export default {
   name: "App",
@@ -12,6 +13,11 @@ export default {
     const width = document.documentElement.clientWidth;
     const menuVisible = ref(width <= 500 ? false : true);
     provide("menuVisible", menuVisible); // set
+    router.afterEach(() => {
+      if (width <= 500) {
+        menuVisible.value = false;
+      }
+    });
   },
 };
 </script>
