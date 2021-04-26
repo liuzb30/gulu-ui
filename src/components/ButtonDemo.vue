40dda8d27fed82d938d47e08c9335dd3375b6767
liu zhenbin <536543269@qq.com>
2021-04-26 15:33:27 +0800

    Button 支持size 属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -7,6 +7,22 @@
     <Button theme="link">你好</Button>
     <Button theme="text">你好</Button>
   </div>
+  <h1>示例2</h1>
+  <div>
+    <Button size="big">大按钮</Button>
+    <Button>中按钮</Button>
+    <Button size="small">小按钮</Button>
+  </div>
+  <div>
+    <Button theme="link" size="big">大链接</Button>
+    <Button theme="link">中链接</Button>
+    <Button theme="link" size="small">小链接</Button>
+  </div>
+  <div>
+    <Button theme="text" size="big">大文本</Button>
+    <Button theme="text">中文本</Button>
+    <Button theme="text" size="small">小文本</Button>
+  </div>
 </template>
 <script lang="ts">
 import Button from "../lib/Button.vue";
