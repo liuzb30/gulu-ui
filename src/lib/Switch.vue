82ea84f27e767c798ba73e336c2be2b30aaf8bb6
liu zhenbin <536543269@qq.com>
2021-04-26 14:07:04 +0800

    Switch 代码优化

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -23,36 +23,38 @@ button {
   height: $h;
   width: $h * 2;
   border: none;
-  background-color: #bfbfbf;
+  background: #bfbfbf;
   border-radius: $h/2;
   position: relative;
-  outline: none;
-}
-span {
-  position: absolute;
-  left: 2px;
-  top: 2px;
-  width: $h2;
-  height: $h2;
-  background-color: #fff;
-  border-radius: $h2/2;
-  transition: all 250ms;
-}
-button.checked {
-  background-color: #1890ff;
-  span {
-    left: calc(100% - #{$h2} - 2px);
-  }
-}
-button:active {
   > span {
-    width: $h2 + 4px;
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: $h2;
+    width: $h2;
+    background: white;
+    border-radius: $h2 / 2;
+    transition: all 250ms;
   }
-}
-button.checked:active {
-  > span {
-    width: $h2 + 4px;
-    margin-left: -4px;
+  &.checked {
+    background: #1890ff;
+    > span {
+      left: calc(100% - #{$h2} - 2px);
+    }
+  }
+  &:focus {
+    outline: none;
+  }
+  &:active {
+    > span {
+      width: $h2 + 4px;
+    }
+  }
+  &.checked:active {
+    > span {
+      width: $h2 + 4px;
+      margin-left: -4px;
+    }
   }
 }
 </style>
