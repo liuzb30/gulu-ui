0577425f1165cacd46290abc882f854f4420de84
liu zhenbin <536543269@qq.com>
2021-04-26 11:15:17 +0800

    Switch组件样式

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -6,5 +6,28 @@
 export default {};
 </script>
 
-<style>
+<style lang="scss" scoped>
+$h: 22px;
+$h2: $h - 4px;
+button {
+  height: $h;
+  width: $h * 2;
+  border: none;
+  background-color: blue;
+  border-radius: $h/2;
+  position: relative;
+  outline: none;
+}
+span {
+  position: absolute;
+  left: 2px;
+  top: 2px;
+  width: $h2;
+  height: $h2;
+  background-color: #fff;
+  border-radius: $h2/2;
+}
+button:hover > span {
+  left: calc(100% - #{$h2} - 2px);
+}
 </style>
