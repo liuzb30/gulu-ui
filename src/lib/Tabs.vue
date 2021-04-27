c3704b72631f7d8c5d5d1af2817b23158e96cac4
liu zhenbin <536543269@qq.com>
2021-04-27 10:26:30 +0800

    Tabs组件  css1

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,10 +1,23 @@
 <template>
-  <div v-for="(title, index) in titles" :key="index">{{ title }}</div>
-  <component
-    v-for="(component, index) in defaults"
-    :is="component"
-    :key="index"
-  ></component>
+  <div class="gulu-tabs">
+    <div class="gulu-tabs-nav">
+      <div
+        class="gulu-tabs-nav-item"
+        v-for="(title, index) in titles"
+        :key="index"
+      >
+        {{ title }}
+      </div>
+    </div>
+    <div class="gulu-tabs-content">
+      <component
+        class="gulu-tabs-content-item"
+        v-for="(component, index) in defaults"
+        :is="component"
+        :key="index"
+      ></component>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -24,5 +37,29 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.gulu-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+  &-content {
+    padding: 8px 0;
+  }
+}
 </style>
