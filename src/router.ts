07e6f8e0eaabf494c5f54be3a962f4edb247dcf0
liu zhenbin <536543269@qq.com>
2021-04-27 15:55:09 +0800

    创建路由

--- src/router.ts
+++ src/router.ts
@@ -6,6 +6,9 @@ import ButtonDemo from './components/ButtonDemo.vue'
 import DialogDemo from './components/DialogDemo.vue'
 import TabsDemo from './components/TabsDemo.vue'
 import DocDemo from './components/DocDemo.vue'
+import GetStarted from './views/GetStarted.vue'
+import Install from './views/Install.vue'
+import Intro from './views/Intro.vue'
 
 const history = createWebHashHistory()
 const router = createRouter({
@@ -17,6 +20,9 @@ const router = createRouter({
         component: Doc,
         children:[
             { path: "", component: DocDemo },
+            {path:'get-started', component:GetStarted},
+            {path:'install', component:Install},
+            {path:'intro', component:Intro},
             {path:'switch', component:SwitchDemo},
             {path:'button', component:ButtonDemo},
             {path:'dialog', component:DialogDemo},
