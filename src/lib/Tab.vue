3a7c8fde978fc82e88b18f7fe1d2ebd131f07832
liu zhenbin <536543269@qq.com>
2021-04-27 10:20:42 +0800

    Tabs组件 渲染Tabs内容

--- src/lib/Tab.vue
+++ src/lib/Tab.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>Tab组件</div>
+  <div><slot /></div>
 </template>
 
 <script>
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,9 +1,27 @@
 <template>
-  <div>Tabs组件</div>
+  <div v-for="(title, index) in titles" :key="index">{{ title }}</div>
+  <component
+    v-for="(component, index) in defaults"
+    :is="component"
+    :key="index"
+  ></component>
 </template>
 
 <script>
-export default {};
+import Tab from "./Tab.vue";
+export default {
+  setup(props, context) {
+    const defaults = context.slots.default();
+    // 检查是否都是Tab组件
+    defaults.forEach((tag) => {
+      if (tag.type !== Tab) {
+        throw new Error("Tabs子标签必须是Tab");
+      }
+    });
+    const titles = defaults.map((tag) => tag.props.title);
+    return { defaults, titles };
+  },
+};
 </script>
 
 <style>
