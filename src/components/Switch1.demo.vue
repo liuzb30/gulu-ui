7ef437423341fa44c90c81700de3c95b13d2c4d7
liu zhenbin <536543269@qq.com>
2021-04-27 17:22:52 +0800

    抽离用户需要拷贝的代码

---
+++ src/components/Switch1.demo.vue
@@ -0,0 +1,18 @@
+<template>
+  <Switch v-model:value="bool" />
+</template>
+
+<script>
+import { ref } from "vue";
+import Switch from "../lib/Switch.vue";
+export default {
+  components: { Switch },
+  setup() {
+    const bool = ref(false);
+    return { bool };
+  },
+};
+</script>
+
+<style>
+</style>
